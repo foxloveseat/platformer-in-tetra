fb544f3c38d1f37d957f7dd792f8dcee2e4a2f08
FoxLovesEat <matewbobkov@gmail.com>
2024-05-30 10:16:20 +0300

    add player

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use tetra::{
     graphics::{self, *},
     input::*,
-    math::{self, vec2, Vec2},
+    math::{self, vec2, FrustumPlanes, Vec2},
     window, Context, ContextBuilder, State,
 };
 
@@ -12,17 +12,33 @@ fn main() -> tetra::Result {
         .run(GameState::new)
 }
 
-struct GameState {}
+struct GameState {
+    player: Player
+}
 
-impl State for GameState {}
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        Ok(())
+    }
+}
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
-        Ok(GameState {})
+        let player = Player::new();
+        Ok(GameState {player})
     }
 }
 
 struct Player {
     position: Vec2<f32>,
     velocity: Vec2<f32>,
+}
+
+
+impl Player {
+    fn new() -> Player {
+        let position = Vec2::new(0., 0.);
+        let velocity = Vec2::new(0., 0.);
+        Player {position: position, velocity: velocity}
+    }
 }
