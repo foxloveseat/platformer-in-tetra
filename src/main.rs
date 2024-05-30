0e1c7bd215acb6e34ea23e305f877830f8bff5c3
FoxLovesEat <matewbobkov@gmail.com>
2024-05-30 10:07:58 +0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+
+use tetra::*;
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Hello", 900, 800)
+        .quit_on_escape(true)
+        .build()?
+        .run(GameState::new)
 }
+
+struct GameState {}
+
+impl State for GameState {}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        Ok(GameState {})
+    }
+}
+
+
+struct Player {
+    position: Vec2<f32>,
+    velocity: Vec2<f32>
+}
+
+
